8c578274df9cc55fbfaaaeba11a869037ddd9d24
nicholasboari <nicholasboari@gmail.com>
2025-07-05 13:40:13 -0300

    feat: estrutura inicial do projeto

---
+++ src/main/java/projeto_final/sistema_hospitalar/SistemaHospitalarApplication.java
@@ -0,0 +1,13 @@
+package projeto_final.sistema_hospitalar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaHospitalarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaHospitalarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/projeto_final/sistema_hospitalar/SistemaHospitalarApplicationTests.java
@@ -0,0 +1,13 @@
+package projeto_final.sistema_hospitalar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaHospitalarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
